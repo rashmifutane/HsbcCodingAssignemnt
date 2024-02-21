a49f9208fbce4701c156f815e625cb73804b44a9
rashmifutane <rashmi.futane89@gmail.com>
2024-02-21 11:41:58 +0530

    Create AsciiFileOperationTest.java

---
+++ AsciiFileOperationTest.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.junit.Test;
+
+public class AsciiFileOperationTest {
+
+    @Test
+    public void shouldReadReverseAndWriteToOutputFile() throws Exception{
+        AsciiFileOperation.reverseContent();
+    }
+
+}
