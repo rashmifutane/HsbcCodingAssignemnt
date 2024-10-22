1e359d4d1ef2398055fed2bb854c9b3de0477ca6
rashmifutane <rashmi.futane89@gmail.com>
2024-10-22 14:51:13 +0530

    Update AsciiFileOperationTest.java

--- AsciiFileOperationTest.java
+++ AsciiFileOperationTest.java
@@ -4,9 +4,26 @@ import org.junit.Test;
 
 public class AsciiFileOperationTest {
 
+   @Test
+    public void testReverseSingleLine() {
+        assertEquals( "!dlroW ,olleH", FileProcessor.reverseContent("Hello, World!"));
+    }
+
     @Test
-    public void shouldReadReverseAndWriteToOutputFile() throws Exception{
-        AsciiFileOperation.reverseContent();
+    public void testReverseMultipleLines() {
+        String input = "Hello, World!\nWelcome to TDD.\n";
+        String expectedOutput = "\n.DDT ot emocleW\n!dlroW ,olleH";
+        assertEquals( expectedOutput, FileProcessor.reverseContent(input));
+    }
+
+    @Test
+    public void testReverseEmptyString() {
+        assertEquals( "", FileProcessor.reverseContent(""));
+    }
+
+    @Test
+    public void testReverseSpecialCharacters() {
+        assertEquals("%$#@!54321", FileProcessor.reverseContent("12345!@#$%"));
     }
 
 }
